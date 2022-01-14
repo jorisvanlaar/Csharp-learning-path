69423e9d662dafb0125b29f62902fae2a5662e74
Joris van Laar <jorisvanlaar@gmail.com>
2022-01-14 15:32:48 +0100

    Basic User Input

---
+++ 2_BasicUserInput/GreetingApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Welcome to the Greeting App!");
+Console.WriteLine("This application was built by Joris van Laar");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+Console.Write("Please enter your first name: ");
+string firstName = Console.ReadLine();
+Console.WriteLine($"Hello { firstName }");
+Console.ReadLine();
