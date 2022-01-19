adc2bcbd9d34859e1766697c44e5b267f4d52363
Joris van Laar <jorisvanlaar@gmail.com>
2022-01-19 21:00:34 +0100

    int variable

---
+++ 3_CommonVariables/IntVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int age = 0;
+age = 43;
+
+int ageInTenYears = age + 10;
+
+int cutOffDecimal = age / 2;            
+
+Console.WriteLine(age);
+Console.WriteLine(ageInTenYears);
+Console.WriteLine(cutOffDecimal);
+
+Console.ReadLine();
