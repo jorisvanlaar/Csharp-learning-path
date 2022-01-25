502c0eb610f5517c0465249722b98f3a2fbb1856
Joris van Laar <jorisvanlaar@gmail.com>
2022-01-25 21:59:26 +0100

    DateTime

---
+++ 4_DateTimeVariables/DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;             
+
+DateTime today = DateTime.Now;          
+Console.WriteLine(today);
+Console.WriteLine(today.ToString("d")); 
+Console.WriteLine(today.ToString("t")); 
+
+DateTime birthday = DateTime.Parse("10/18/1985");
+Console.WriteLine(birthday);                        
+
+birthday = DateTime.ParseExact("18/10/1985", "d/M/yyyy", CultureInfo.InvariantCulture);
+Console.WriteLine(birthday);
+
+DateTime today2 = DateTime.Now;
+Console.WriteLine(today2.ToString("MMMM dd, yyyy hh:mm tt zzz"));    
+                                                                     
+DateTime today3 = DateTime.UtcNow;
+Console.WriteLine(today3.ToString("MMMM dd, yyyy hh:mm tt zzz"));
+
+Console.ReadLine();
