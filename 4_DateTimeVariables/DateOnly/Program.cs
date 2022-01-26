461a4e5bd28393fb24c39a1375f4dc6008ee8f5e
Joris van Laar <jorisvanlaar@gmail.com>
2022-01-26 20:49:10 +0100

    DateOnly & TimeOnly

---
+++ 4_DateTimeVariables/DateOnly/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+DateTime today = DateTime.Now;
+Console.WriteLine($"Just the date from today: {today.Date}");   
+
+DateOnly birthday = DateOnly.Parse("10/18/1985");       
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));  
---
+++ 4_DateTimeVariables/TimeOnly/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
+Console.WriteLine(opensAt);
+
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+Console.WriteLine(rightNow);
