5d1cab26d6228167057521f6181aa838c56886d1
Joris van Laar <jorisvanlaar@gmail.com>
2022-01-27 18:12:07 +0100

    Type Conversion

---
+++ 5_TypeConversion/Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+
+bool isValidAge = int.TryParse(ageText, out int age);
+
+if (isValidAge)
+{
+    Console.WriteLine($"You were {age - 25} 25 years ago.");
+    Console.WriteLine($"You will be {age + 25} in 25 years.");
+}
+else
+{
+    Console.WriteLine("That is not a valid age.");
+}
---
+++ 5_TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+
+// Conversion with Parse
+int age = int.Parse(ageText);
+Console.WriteLine($"Your age in 10 years: {age + 10}");
+
+// Conversion with TryParse
+int age2;                                                    
+bool isValidAge = int.TryParse(ageText, out age2);           
+
+if (isValidAge)
+{
+    Console.WriteLine($"Your age in 10 years: {age2 + 10}");
+}
+else
+{
+    Console.WriteLine("That's an invalid number");
+}
+
+bool isValidAge3 = int.TryParse(ageText, out int age3);                                                                 
+Console.WriteLine($"age3: {age3}");                         
+
+// Implicit conversion
+int numberInt = 50;                 
+double numberDouble = numberInt;    
+                                    
+// Casting
+decimal numberDecimal = (decimal)numberDouble;
