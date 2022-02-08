55151d0dc362454b1e50dbc3f2e6b0f6dda89c54
Joris van Laar <jorisvanlaar@gmail.com>
2022-02-08 17:21:43 +0100

    Do While Loops

---
+++ 8_LoopsAndSets/DoWhileLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+bool isValidAge;
+int age;
+
+// Do While Loop
+do
+{
+    Console.Write("What is your age: ");
+    string? ageText = Console.ReadLine();
+    isValidAge = int.TryParse(ageText, out age);
+
+    if (isValidAge == false)
+    {
+        Console.WriteLine("That was an invalid age");
+    }
+} while (isValidAge == false);
+
+Console.WriteLine($"Your age is {age}");
+
+
+
+Console.Write("What is your age: ");
+string? ageText2 = Console.ReadLine();
+bool isValidAge2 = int.TryParse(ageText2, out int age2);
+
+// While Loop
+while (isValidAge2 == false)
+{
+    Console.Write("What is your age: ");
+    string? ageText = Console.ReadLine();
+    isValidAge2 = int.TryParse(ageText, out age2);
+}
+
+Console.WriteLine($"Your age is {age2}");
---
+++ 8_LoopsAndSets/DoWhileLoops_Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string? exit;
+
+do
+{   
+    Console.Write("What is your first name: ");
+    string? firstName = Console.ReadLine();
+
+    if (firstName.ToLower() == "tim")
+    {
+        Console.WriteLine($"Welcome Professor {firstName}");
+    }
+    else
+    {
+        Console.WriteLine($"Welcome {firstName}");
+    }
+
+    Console.Write("Type 'exit' if you want to stop: ");
+    exit = Console.ReadLine();
+
+} while (exit.ToLower() != "exit");
