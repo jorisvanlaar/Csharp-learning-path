217573f1b971297960c3e1e5b3f965bfd03ded93
Joris van Laar <jorisvanlaar@gmail.com>
2022-02-12 20:53:13 +0100

    Array

---
+++ 8_LoopsAndSets/Array/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string[] firstNames = new string[5];    
+
+firstNames[0] = "Tim";                  
+firstNames[1] = "Sue";
+firstNames[2] = "Bob";
+firstNames[4] = "Jane";
+
+Console.WriteLine($"The first firstName is: {firstNames[0]}");  
+firstNames[0] = "Timothy";              
+
+string data = "Tim,Sue,Bob,Jane,Frank";     
+string[] firstNamesCsv = data.Split(',');   
+Console.WriteLine(firstNamesCsv[0]);        
+Console.WriteLine(firstNamesCsv.Length);    
+Console.WriteLine(firstNamesCsv[firstNamesCsv.Length - 1]); 
+
+string[] lastNames = new string[] { "Corey", "Smith", "Jones" };
+
+int[] ages = new int[] { 2, 3, 4 };
+
---
+++ 8_LoopsAndSets/Array_Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string[] firstNames = new string[] { "Bas", "Stefan", "Joris" };
+
+bool isValidNumber;
+int number;
+
+do
+{
+    Console.Write("Please choose a number between 0 and 2: ");
+    string? numberText = Console.ReadLine();
+
+    isValidNumber = int.TryParse(numberText, out number);
+
+} while (isValidNumber == false || (number < 0 || number > 2));
+
+Console.WriteLine(firstNames[number]);
