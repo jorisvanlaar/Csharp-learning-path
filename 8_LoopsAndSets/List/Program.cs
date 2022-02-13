6f96963dfb72d8e7af7be86004ec0837f411377f
Joris van Laar <jorisvanlaar@gmail.com>
2022-02-13 17:51:42 +0100

    List

---
+++ 8_LoopsAndSets/List/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+List<string> firstNames = new List<string>();
+
+firstNames.Add("Tim");              
+firstNames.Add("Sue");
+firstNames.Add("Bob");
+firstNames.Add("Jane");
+
+Console.WriteLine(firstNames[0]);                  
+Console.WriteLine(firstNames[firstNames.Count - 1]);    
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+string data = "Corey,Smit,Jones";
+List<string> lastNames = data.Split(',').ToList();
---
+++ 8_LoopsAndSets/List_Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+List<string> classRoster = new List<string>();
+string anotherStudent;
+
+do
+{
+    Console.Write("Please enter the student's first name: ");
+    string? studentName = Console.ReadLine();
+
+    if (studentName != "")
+    {
+        classRoster.Add(studentName);
+    }
+
+    Console.Write("Do you want to enter another student (y/n): ");
+    anotherStudent = Console.ReadLine();
+
+} while (anotherStudent.ToLower() == "y");
+
+Console.WriteLine($"Number of students: {classRoster.Count}");
