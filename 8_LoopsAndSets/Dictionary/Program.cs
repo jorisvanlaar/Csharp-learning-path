c0868712c126ae558c15dea82ed2f16a5ea06e75
Joris van Laar <jorisvanlaar@gmail.com>
2022-02-15 08:55:04 +0100

    Dictionary

---
+++ 8_LoopsAndSets/Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Dictionary<string, string> lookup = new Dictionary<string, string>();
+
+lookup["animal"] = "Not a human";
+lookup["fish"] = "Not a human that swims";
+lookup["human"] = "Us";
+lookup.Add("bird", "Not a human that flies");   
+
+Console.WriteLine($"The definition of fish is: {lookup["fish"]}");
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[95] = "Joris van Laar";
+employees[28] = "Sue Storm";
+
+Console.WriteLine($"The employee with ID 28: {employees[28]} ");
---
+++ 8_LoopsAndSets/Dictionary_Homework/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+employees.Add(1, "Joris van Laar");
+employees.Add(2, "Bas Roding");
+employees.Add(3, "Stefan Mensen");
+employees.Add(4, "Stefan Wijnker");
+employees.Add(5, "Mark Bouwman");
+employees.Add(6, "Eamon Woortman");
+
+Console.Write("What is your employee ID: ");
+string? idText = Console.ReadLine();
+
+bool isValidId = int.TryParse(idText, out int id);
+
+if (isValidId && (id > 0 && id < 7))
+{
+    Console.WriteLine($"Hello {employees[id]}");
+}
