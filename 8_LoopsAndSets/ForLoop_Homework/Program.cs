f1451a61ed229bfa2ddaf2a8121ae2c12ab86df3
Joris van Laar <jorisvanlaar@gmail.com>
2022-02-19 21:16:02 +0100

    For Loop

---
+++ 8_LoopsAndSets/ForLoop_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Please enter a comma-separated list of first names: ");
+string? names = Console.ReadLine();
+string[] firstNames = names.Split(',');
+
+for (int i = 0; i < firstNames.Length; i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}");
+}
