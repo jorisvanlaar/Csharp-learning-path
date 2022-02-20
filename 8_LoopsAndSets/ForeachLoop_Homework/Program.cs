5a9736ad301fce45e3dce0851d9a319cf7000ba8
Joris van Laar <jorisvanlaar@gmail.com>
2022-02-20 13:13:44 +0100

    Foreach loop

---
+++ 8_LoopsAndSets/ForeachLoop_Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+List<string> firstNames = new List<string>();
+string? firstName;
+
+do
+{
+    Console.Write("Enter a first name (type 'exit' to quit): ");
+    firstName = Console.ReadLine();
+
+    if (firstName != "" && firstName.ToLower() != "exit")
+    {
+        firstNames.Add(firstName);
+    }
+} while (firstName.ToLower() != "exit");
+
+foreach (string name in firstNames)
+{
+    Console.WriteLine($"Hello {name}");
+}
