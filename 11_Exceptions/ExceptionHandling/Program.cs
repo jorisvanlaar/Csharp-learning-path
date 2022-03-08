089f49f823b147e319602fd1c102f8c0e0c36bbb
Joris van Laar <jorisvanlaar@gmail.com>
2022-03-08 21:04:25 +0100

    Exception Handling

---
+++ 11_Exceptions/ExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+try
+{
+    BadCall();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Hi user, there was an error: {ex.Message}");
+}
+
+static void BadCall()
+{
+    int[] ages = new int[] { 1, 3, 5 };
+
+    for (int i = 0; i <= ages.Length; i++)
+    {
+        try
+        {
+            Console.WriteLine(ages[i]);
+        }
+        catch (Exception ex)                            
+        {
+            Console.WriteLine("Handle the exception");
+            throw;          
+        }
+    }
+}
---
+++ 11_Exceptions/ExceptionHandling_Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+try
+{
+    ExceptionMethod();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+static void ExceptionMethod()
+{
+    int[] ages = { 34, 45, 378, 765, 9 };
+
+    for (int i = 0; i <= ages.Length; i++)
+    {
+        Console.WriteLine(ages[i]);
+    }
+}
