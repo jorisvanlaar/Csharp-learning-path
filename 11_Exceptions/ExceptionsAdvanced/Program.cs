806e75cfdde823dd89666f6fb9d5df8eae7f5f9c
Joris van Laar <jorisvanlaar@gmail.com>
2022-03-15 21:18:38 +0100

    Advanced exception handling

---
+++ 11_Exceptions/ExceptionsAdvanced/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+string? name = "";
+
+try
+{
+    SimpleMethod();                     
+    DifferentMethod();                  
+                                        
+    Console.Write("What is your name: ");
+    name = Console.ReadLine();
+    ComplexMethod(name);                                                                 
+}
+catch (InvalidOperationException ex)    
+{                                       
+    Console.WriteLine("Handling an InvalidOperationsException");
+    Console.WriteLine(ex.Message);
+}
+catch (NotImplementedException ex)       
+{
+    Console.WriteLine("Handling a NotImplementedException");
+    Console.WriteLine(ex.Message);
+}
+catch (Exception) when (name.ToLower() == "joris")              
+{                                                                                                                       
+    Console.WriteLine("You used Joris' name, didn't you?");
+}
+catch (Exception ex)                    
+{                                       
+    Console.WriteLine(ex.Message);      
+}
+finally                                         
+{                                                                                     
+    Console.WriteLine("I always run");    
+}
+
+
+static void SimpleMethod()
+{
+    throw new InvalidOperationException("You should not be calling the SimpleMethod");
+}
+
+static void DifferentMethod()
+{
+    throw new NotImplementedException("You should not be calling the DifferentMethod");
+}
+
+static void ComplexMethod(string name)
+{
+    if (name.ToLower() == "joris")
+    {
+        throw new InsufficientMemoryException("Joris is too big for this method");
+    }
+    else
+    {
+        throw new ArgumentException("This person isn't Joris");
+    }
+}
---
+++ 11_Exceptions/ExceptionsAdvanced_Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.Write("What is your name: ");
+string? name = Console.ReadLine();
+
+try
+{
+    ExceptionMethod();
+}
+catch (NotImplementedException ex)
+{
+    Console.WriteLine("The dev has forgotten to complete his code!");
+    Console.WriteLine(ex.Message);
+}
+catch (Exception) when (name.ToLower() == "joris")
+{
+    Console.WriteLine("Something went wrong, but your name is Joris");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.WriteLine("Our apologies");
+}
+
+void ExceptionMethod()
+{
+    throw new NotImplementedException();
+}
