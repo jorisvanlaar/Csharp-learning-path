c4bab8bf7cb63ff572c11ea7f4c0e68c6ee77e4b
Joris van Laar <jorisvanlaar@gmail.com>
2022-04-24 21:39:07 +0200

    Instantiated Classes

---
+++ 13_InstantiatedClasses/Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿using Homework;
+
+AddressModel address = new();
+address.Street = "Surinameplein";
+address.HouseNumber = "39-3";
+address.ZipCode = "1058GM";
+address.City = "Amsterdam";
+address.Country = "Netherlands";
+
+PersonModel person = new();
+person.FirstName = "Joris";
+person.LastName = "van Laar";
+person.Age = 36;
+person.Address = address;
+
+Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age} years old.");
+Console.WriteLine($"Lives at {person.Address.Street} {person.Address.HouseNumber}, {person.Address.ZipCode}");
+Console.WriteLine($"{person.Address.City}, {person.Address.Country}");
---
+++ 13_InstantiatedClasses/InstantiatedClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿using InstantiatedClasses;  
+
+
+List<PersonModel> people = new();
+string firstName = "";
+
+do
+{
+    Console.Write("What is your first name (or type exit to stop): ");
+    firstName = Console.ReadLine();
+
+    if (firstName.ToLower() != "exit")
+    {
+        Console.Write("What is your last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("What is your email: ");
+        string emailAddress = Console.ReadLine();
+        Console.WriteLine();
+
+        PersonModel person = new();
+        person.FirstName = firstName;
+        person.LastName = lastName;
+        person.EmailAddress = emailAddress;
+        people.Add(person);
+    }
+} while (firstName.ToLower() != "exit");
+
+foreach (PersonModel p in people)
+{
+    ProcessPerson.GreetPerson(p);   
+}
