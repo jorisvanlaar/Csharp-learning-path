f479015a8b53c668926b33c561be23cb4c9ec044
Joris van Laar <jorisvanlaar@gmail.com>
2022-06-01 22:48:54 +0200

    Class Libraries

---
+++ 16_ClassLibraries/ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using DemoLibrary;
+using DemoLibrary.Models;
+
+
+Calculation.Add(2, 3);     
+                                
+PersonModel person = new("Joris", "van Laar");     
+
+string firstName = UserMessages.GetString("What is your first name: "); 
+string lastName = UserMessages.GetString("What is your last name: ");
+PersonModel person2 = new(firstName, lastName);
+UserMessages.Greet(person2.FirstName, person2.LastName);    
---
+++ 16_ClassLibraries/DemoLibrary/UserMessages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class UserMessages
+    {
+        public static string GetString(string message)
+        {
+            Console.Write(message);
+            
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        public static void Greet(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello {firstName} {lastName}");
+        }
+    }
+}
