8cd614f8e227660eb6f12a462031785b836829cf
Joris van Laar <jorisvanlaar@gmail.com>
2022-08-08 21:02:39 +0200

    Access Modifiers

---
+++ 22_AccessModifiers/DemoLibrary/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Person
+    {
+        protected string formerLastName = "";           
+
+        public string FirstName { get; private set; }   
+        public string LastName { get; private set; }
+
+        private string _ssn;                    
+
+        public string SSN
+        {
+            get
+            {
+                return $"***-**-{_ssn.Substring(_ssn.Length - 4)}"; 
+            }
+            set { _ssn = value; }
+        }
+
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello {FirstName}");
+        }
+
+        public void ChangeLastName(string newLastName)
+        {
+            formerLastName = LastName;          
+            LastName = newLastName;
+        }
+        
+        public void SavePerson()
+        {
+            DataAccess data = new DataAccess();
+            string connectionString = data.GetConnectionString();
+            // saves the person to the db
+        }
+    }
+}
